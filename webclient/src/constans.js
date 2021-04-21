108c7cbeb38dd42a7ea4df3c2b5f9b25ab6e6921
andregardi <gardikoury@gmail.com>
2021-04-21 18:21:00 -0300

    use cross-env lib to set REACT_APP_SERVER_URL on command line

--- webclient/src/constans.js
+++ webclient/src/constans.js
@@ -1,2 +1,2 @@
-export const ASSETS_SERVER_URL = 'http://localhost:4000'
-export const GRAPHQL_SERVER_URL = 'http://localhost:4000/graphql'
+export const ASSETS_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+export const GRAPHQL_SERVER_URL = process.env.REACT_APP_SERVER_URL + "/graphql";
